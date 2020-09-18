000f2b1c43a2814c182e9481b85440a7db808d1e
Gary Feltham <gary.feltham@citypay.com>
2020-09-18 10:06:12 +0100

    version change

--- citypay/configuration.py
+++ citypay/configuration.py
@@ -380,8 +380,8 @@ conf = citypay.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 6.0.0\n"\
-               "SDK Package Version: 1.0.1".\
+               "Version of the API: 6.0.9\n"\
+               "SDK Package Version: 1.0.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
