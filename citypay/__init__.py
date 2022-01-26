ad59e79055ee6b8f95779cbeb5bf5e7ce8645209
hectorconceicao <hector.conceicao@citypay.com>
2022-01-26 15:14:53 +0000

    . Added __init__.py in tests. . Add models and APIs imports in citypay package

--- citypay/__init__.py
+++ citypay/__init__.py
@@ -25,4 +25,58 @@ from citypay.exceptions import ApiTypeError
 from citypay.exceptions import ApiValueError
 from citypay.exceptions import ApiKeyError
 from citypay.exceptions import ApiException
-from citypay.models.api_key import api_key_generate
+
+from citypay.model.api_key import api_key_generate
+
+# Import APIs into this package:
+from citypay.api.batch_processing_api import BatchProcessingApi
+from citypay.api.card_holder_account_api import CardHolderAccountApi
+from citypay.api.operational_api import OperationalApi
+from citypay.api.payment_processing_api import PaymentProcessingApi
+
+# import all models into this package
+from citypay.model.account_create import AccountCreate
+from citypay.model.account_status import AccountStatus
+from citypay.model.acknowledgement import Acknowledgement
+from citypay.model.acl_check_request import AclCheckRequest
+from citypay.model.acl_check_response_model import AclCheckResponseModel
+from citypay.model.airline_advice import AirlineAdvice
+from citypay.model.airline_segment import AirlineSegment
+from citypay.model.auth_reference import AuthReference
+from citypay.model.auth_references import AuthReferences
+from citypay.model.auth_request import AuthRequest
+from citypay.model.auth_response import AuthResponse
+from citypay.model.authen_required import AuthenRequired
+from citypay.model.batch import Batch
+from citypay.model.batch_report_request import BatchReportRequest
+from citypay.model.batch_report_response_model import BatchReportResponseModel
+from citypay.model.batch_transaction import BatchTransaction
+from citypay.model.batch_transaction_result_model import BatchTransactionResultModel
+from citypay.model.bin import Bin
+from citypay.model.bin_lookup import BinLookup
+from citypay.model.c_res_auth_request import CResAuthRequest
+from citypay.model.capture_request import CaptureRequest
+from citypay.model.card import Card
+from citypay.model.card_holder_account import CardHolderAccount
+from citypay.model.card_status import CardStatus
+from citypay.model.charge_request import ChargeRequest
+from citypay.model.check_batch_status import CheckBatchStatus
+from citypay.model.check_batch_status_response import CheckBatchStatusResponse
+from citypay.model.contact_details import ContactDetails
+from citypay.model.decision import Decision
+from citypay.model.error import Error
+from citypay.model.exists import Exists
+from citypay.model.external_mpi import ExternalMPI
+from citypay.model.list_merchants_response import ListMerchantsResponse
+from citypay.model.mcc6012 import MCC6012
+from citypay.model.merchant import Merchant
+from citypay.model.pa_res_auth_request import PaResAuthRequest
+from citypay.model.ping import Ping
+from citypay.model.process_batch_request import ProcessBatchRequest
+from citypay.model.process_batch_response import ProcessBatchResponse
+from citypay.model.refund_request import RefundRequest
+from citypay.model.register_card import RegisterCard
+from citypay.model.request_challenged import RequestChallenged
+from citypay.model.retrieve_request import RetrieveRequest
+from citypay.model.three_d_secure import ThreeDSecure
+from citypay.model.void_request import VoidRequest
